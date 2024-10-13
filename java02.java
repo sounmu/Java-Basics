c5e9c972e5c74c5d2d8995cebd107016eb48644d
sounmu <hyginus011111@gmail.com>
2024-10-13 16:34:37 +0900

    feat: add 2장 자바 시작하기

---
+++ java02.java
@@ -0,0 +1,56 @@
+/* 클래스 블록  */
+public class java02 {
+    /*
+     * 여러 메서드 블록을 포함할 수 있다.
+     * [public|private|protected] [static] (리턴자료형|void) 메서드명1 (파라미터) {
+     *      명령문;
+     *      ...
+     * }
+     */
+
+     /*
+      * 속성과 생성자
+        [속성]
+        private String message;
+        클래스의 상태를 나타내는 변수, 클래스 내부에 선언
+
+        [생성자]
+        public Sample(String message) {
+            this.message = message
+        }
+        클래스 인스턴스를 생성할 때 실행되는 메서드, 주로 속성을 초기화.
+      */
+}
+
+
+/* 클래스명과 소스 파일 이름은 같아야 함 */
+
+/* 자료형 */
+
+/*
+ * int, long, double
+ * boolean
+ * char, String, StringBuffer
+ * List, Map, Set
+ */
+
+ /* 사용자 정의 자료형 */
+
+ /*
+  * class Animal {
+    }
+
+    Animal cat;
+  */
+
+  /* 클래스 이름 짓기 */
+
+  /*
+   * 명사, 대문자로 시작. 여러 개의 단어로 조합할 경우 첫 글자는 대문자.
+   */
+
+   /* 메서드 이름 짓기 */
+
+   /*
+    * 동사. 소문자. camel case. 맨 첫 글자를 제외한 나머지 단어의 첫 글자는 대문자를 써서 구분.
+    */
